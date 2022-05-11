df860ef003d68ec5e3031ead65a8445d730d02f8
Felipe Bassi <fkb5105@psu.edu>
2022-05-11 18:20:18 -0300

    This was my design for the homepage layout

---
+++ docs/DIGIT.js
@@ -0,0 +1,8 @@
+function myFunction(menubar) {
+  var x = document.getElementById("myMenu");
+  if (x.className === "menu") {
+    x.className += " responsive";
+  } else {
+    x.className = "menu";
+  }
+}
