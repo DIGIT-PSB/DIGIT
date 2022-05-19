bbdfd037e23117d7827392d66393e5ad11dd4b58
William Stiller <wxs5242@psu.edu>
2022-05-19 17:00:16 -0400

    focus class completed, project.js added

---
+++ docs/project.js
@@ -0,0 +1,20 @@
+const projects = document.getElementsByClassName("work")
+
+window.onload = init(); 
+
+
+document.getElementsByClassName
+
+/*  
+function addFocus() {
+
+  var x = document.getElementById("other");
+  if (x.className === "work") {
+    x.className += "focus";
+    console.log("Added")
+  } else {
+    x.className = "work";
+    console.log("Removed")
+  }
+}
+ */
