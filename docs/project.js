02a742d980148908d7e7c2a22d91968ec87cf0ce
ebeshero <ebbondar@gmail.com>
2022-05-23 01:04:16 -0400

    JS classList and css flex for showcase

--- docs/project.js
+++ docs/project.js
@@ -1,20 +1,30 @@
 const projects = document.getElementsByClassName("work")
 
-window.onload = init(); 
+window.onload = init;
 
-
-document.getElementsByClassName
-
-/*  
-function addFocus() {
-
-  var x = document.getElementById("other");
-  if (x.className === "work") {
-    x.className += "focus";
-    console.log("Added")
-  } else {
-    x.className = "work";
-    console.log("Removed")
-  }
+function init() {
+    for (var i = 0; i < projects.length; i++) {
+        projects[i].addEventListener('click', expand, false);
+    }
 }
+
+/* 2022-05-23 ebb Source: https://www.geeksforgeeks.org/how-to-make-the-images-bigger-when-clicked/  */
+function expand() {
+    console.log('Yay I am firing');
+    this.classList.toggle("focus");
+    
+}
+
+/*
+ function addFocus() {
+
+ var x = document.getElementById("other");
+ if (x.className === "work") {
+ x.className += "focus";
+ console.log("Added")
+ } else {
+ x.className = "work";
+ console.log("Removed")
+ }
+ }
  */
